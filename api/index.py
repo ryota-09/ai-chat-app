09f37a11a7fad5b6276738a88e6329fd8035c329
ryota-09 <n.ryo950911@gmail.com>
2024-01-21 21:06:18 +0900

    openaiの箇所をコメントアウト

--- api/index.py
+++ api/index.py
@@ -13,11 +13,11 @@ app = FastAPI()
 
 @app.get("/api/python")
 async def hello_world():
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": "大谷翔平について教えて"},
-        ],
-    )
-    text = response.choices[0]["message"]["content"]
-    return {"message": text}
+    # response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "user", "content": "大谷翔平について教えて"},
+    #     ],
+    # )
+    # text = response.choices[0]["message"]["content"]
+    return {"message": "Hello World"}
