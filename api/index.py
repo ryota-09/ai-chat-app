3fb68ebfd46860d563e506b88ba65a21261a6541
ryota-09 <n.ryo950911@gmail.com>
2024-01-21 20:27:14 +0900

    不要なコードを削除

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+text = os.getenv("TEXT")
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"message": text}
