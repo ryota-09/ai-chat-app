ba5fb14bf5134197a2822ce00f36a6b156220b3d
ryota-09 <n.ryo950911@gmail.com>
2024-01-28 14:15:38 +0900

    メッセージidを取得する関数の作成

--- util/index.ts
+++ util/index.ts
@@ -43,3 +43,14 @@ export const getChatId = () => {
 
   return `chat-${year}${month}${day}${hours}${minutes}_${uuid()}`;
 }
+
+export const getMessageId = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+
+  return `message-${year}${month}${day}${hours}${minutes}_${uuid()}`;
+}
